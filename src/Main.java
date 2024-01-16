dfef6e4b836e8cedf377352e7a4d40d8e8e4ba18
toto <antrim246@gmail.com>
2024-01-15 23:00:57 -0500

    added switch case for operations and error handling for division by zero

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+/* Anthony Karim
+*  Lab3: calculatrice
+*  Date de remise: 15 janvier 2024
+* */
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Create a scanner object
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = Integer.parseInt(myScanner.nextLine());
+
+        System.out.print("Enter the second number: ");
+        int num2 = Integer.parseInt(myScanner.nextLine());
+
+        System.out.print("Select an operation (+, -, *, /): ");
+        String operation = myScanner.nextLine();
+
+        int result = 0;
+
+        switch(operation){
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                try {
+                    result = num1 / num2;
+                }
+                catch (Exception e) {
+                    System.out.println("Error: " + e.getMessage());
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation symbol...");
+                return;
+        }
+
+        System.out.println("The result of the operation is: " + result);
+
     }
 }
